5b728a0e7a68e422801dc4e28344f3467cdfab93
Guanbo-W <guanbo.wang@mail.mcgill.ca>
2022-02-09 23:51:41 -0500

    Update Functions_new.R

--- Functions_new.R
+++ Functions_new.R
@@ -1,4 +1,4 @@
-
+# generate B
 gen_con=function(m){
   X=rnorm(m/5)
   XX=NULL
@@ -11,6 +11,7 @@ gen_con=function(m){
   return(XX[1:m])
 }
 
+# generate A and C
 gen_cat=function(m){
   X=sample.int(3, m/5,replace = TRUE)
   XX=NULL
@@ -23,6 +24,7 @@ gen_cat=function(m){
   return(XX[1:m])
 }
 
+# generate covariate for one subject
 gen_X=function(m){
   A=gen_cat(m);B=gen_con(m);C=gen_cat(m)
   A1=ifelse(A==1,1,0);A2=ifelse(A==2,1,0)
@@ -32,6 +34,7 @@ gen_X=function(m){
   return(as.matrix(cbind(A1,A2,C1,C2,B,A1B,A2B,C1B,C2B)))
 }
 
+# generate covariate for all subject
 gen_X_n=function(m,n){
   Xn=NULL
   for (i in 1:n) {
@@ -41,42 +44,20 @@ gen_X_n=function(m,n){
   return(Xn)
 }
 
-
-# data <- permalgorithm(n, m, Xmat=gen_X_n(m,n),
-#                       XmatNames=c("A1","A2","B","C1","C2","A1B","A2B","C1B","C2B"),
-#                       #eventRandom = eventRandom, censorRandom=censorRandom,
-#                       betas=c(log(3), log(3), rep(0,7)),groupByD=FALSE )
-# coxph(Surv(Start, Stop, Event) ~ A1+A2+B+C1+C2+A1B+A2B+C1B+C2B,data)
-# #lam1=2;regul='graph';betas=rep(0,5)  
-# n=50;m=50
+# loss function and its first derivative
 fit.cox=function(beta,data){coxph(Surv(Start, Stop, Event) ~ .,data,init = beta, control=list('iter.max'=0,timefix = FALSE))}
 l=function(beta,data){-fit.cox(beta,data)$loglik[1]/n}
 l.d=function(beta,data){-fit.cox(beta,data)$first/n}
 
-# dataa <- permalgorithm(n, m, Xmat=gen_X_n(m,n)[,1],
-#                       XmatNames=c("A1"),
-#                       #eventRandom = eventRandom, censorRandom=censorRandom,
-#                       betas=c(log(3)),groupByD=FALSE )
-# coxph(Surv(Start, Stop, Event) ~ A1,dataa)
-# x=as.matrix(seq(0.8,1.5,0.01),byrow=T)
-# y=apply(x, 1, FUN = function(x) l(x,dataa[,-c(1,3)]))
-# z=apply(x, 1, FUN = function(x) -l.d(x,dataa[,-c(1,3)]))
-# plot(x,y);abline(v=1.1966)
-# plot(x,z);abline(v=1.1966,h=0)
-# https://stats.stackexchange.com/questions/187281/how-to-compute-partial-log-likelihood-function-in-cox-proportional-hazards-model 
-
-
+# Proximal gradient descent with backtracking line search
 SurvTDSelect=function(data, grp, grpV, eta_g, regul, betas, t, alpha, epsilon, lam1){
   graph = list('eta_g'= eta_g,'groups' = grp,'groups_var' = grpV)
-  #lambda1=lam1/t
   n=length(unique(data$Id));data=data[,-c(1,3)]
   U=matrix(betas-l.d(betas,data)*t, byrow=FALSE)
   V=spams.proximalGraph(U,graph=graph,lambda1=lam1,regul="graph")
   while(sum(abs(betas-V))>epsilon){
     while( l(V,data) > l(betas,data)+l.d(betas,data) %*% (V-betas)+sum((V-betas)^2)/(2*t) ){
       t = alpha * t
-      #U=matrix(betas-l.d(betas,data)*t, byrow=FALSE)
-      #V=spams.proximalGraph(U,graph=graph,lambda1=lam1/t,regul="graph")
     }
     betas=V
     U=matrix(betas-l.d(betas,data)*t, byrow=FALSE)
@@ -85,8 +66,7 @@ SurvTDSelect=function(data, grp, grpV, eta_g, regul, betas, t, alpha, epsilon, l
   return(V)
 }
 
-
-
+# cross-validation
 SurvTDSelect_CV = function(data,grp, grpV, eta_g, regul, betas, t, alpha, epsilon, lam1, K){
   error=NULL
   graph = list('eta_g'= eta_g,'groups' = grp,'groups_var' = grpV)
@@ -98,12 +78,6 @@ SurvTDSelect_CV = function(data,grp, grpV, eta_g, regul, betas, t, alpha, epsilo
     test.data = data[cv.id == i,]
     train.data = data[cv.id != i,]
     est.train = SurvTDSelect(train.data, grp, grpV, eta_g, regul, betas, t, alpha, epsilon, lam1)
-    #error[i] = -2*n*(-l(est.train,data[,-c(1,3)])+l(est.train,train.data[,-c(1,3)]))
-    #error[i] = 2*n*(l(est.train,test.data[,-c(1,3)]))
-    # fit.original.test = coxph(Surv(Start, Stop, Event) ~ . ,test.data[,-c(1,3)])
-    # error[i] = 2*(fit.original.test$loglik[2]+l(est.train,test.data[,-c(1,3)])*n)
-    # # https://github.com/cran/glmnet/blob/master/R/coxnet.deviance.R line 170
-    # https://arxiv.org/pdf/1905.10432.pdf 
     error[i] = coxnet.deviance(y=Surv(data$Start, data$Stop, data$Event),
                                x=as.matrix(data[,-c(1:5)]),beta=est.train)-
       coxnet.deviance(y=Surv(train.data$Start, train.data$Stop, train.data$Event),
