e46be3f8c3ba8f71eb522b4d302c60c30dd1587e
Guanbo-W <guanbo.wang@mail.mcgill.ca>
2022-02-09 23:44:52 -0500

    Add files via upload

---
+++ new_500_ABAB_K10_steve.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH --time=02:0:00
+#SBATCH --job-name="new_500_ABAB_K10_steve"
+#SBATCH --account=def-dsteph
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=50
+#SBATCH --mem=15G
+
+cd /home/shomoita/guanbo
+
+module --force purge
+module load nixpkgs/16.09  
+module load gcc/8.3.0
+module load StdEnv/2020
+module load tktable/2.10
+module load gcc/9.3.0
+module load r/4.1.2
+
+
+# Export the nodes names. 
+# If all processes are allocated on the same node, NODESLIST contains : node1 node1 node1 node1
+# Cut the domain name and keep only the node name
+export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
+Rscript --vanilla new_500_ABAB_K10_steve.R $1 $2
